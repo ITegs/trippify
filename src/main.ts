95eb29234d9cb2a034985b407786b74dabf5ed1a
Johannes Pahle <joe@jo-dev.de>
2024-02-08 22:12:14 +0100

    📦 Add `@vueuse/gesture`

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,15 @@ import './assets/main.scss'
 import 'leaflet/dist/leaflet.css'
 
 import { createApp } from 'vue'
+import { GesturePlugin } from '@vueuse/gesture'
 import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
+
+app.use(GesturePlugin)
 
 app.use(createPinia())
 app.use(router)
