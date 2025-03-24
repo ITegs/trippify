bf4ea036c0fd7381a62e1224acb23d56512f7c25
Johannes Pahle <joe@jo-dev.de>
2025-03-24 14:53:06 +0100

    Fix add spot image removal function

--- frontend/src/views/AddSpotView.vue
+++ frontend/src/views/AddSpotView.vue
@@ -2,63 +2,78 @@
   <main>
     <div class="head">
       <div class="back" @click="goBack">
-        <font-awesome-icon :icon="['fas', 'chevron-left']"/>
+        <font-awesome-icon :icon="['fas', 'chevron-left']" />
         Zurück
       </div>
       <p>Neuer Spot</p>
     </div>
 
-    <input type="text" v-model="spot.title" placeholder="Wo bist du?" class="location"/>
+    <input type="text" v-model="spot.title" placeholder="Wo bist du?" class="location" />
 
     <div class="map">
-      <SmallMap :position="[spot.latitude, spot.longitude]" :marker="[[spot.latitude, spot.longitude]]"
-                :draggable="false" class="mapC"
-                v-if="spot.latitude !== 0 && spot.longitude !== 0"/>
+      <SmallMap
+        :position="[spot.latitude, spot.longitude]"
+        :marker="[[spot.latitude, spot.longitude]]"
+        :draggable="false"
+        class="mapC"
+        v-if="spot.latitude !== 0 && spot.longitude !== 0"
+      />
       <p v-else>Standort läd...</p>
       <div class="refresh" @click="getLocation">
-        <font-awesome-icon :icon="['fas', 'sync']" inverse/>
+        <font-awesome-icon :icon="['fas', 'sync']" inverse />
       </div>
     </div>
 
     <div class="images">
-      <div v-for="image in spot.images" :id="image.timestamp?.toString()" :key="image.timestamp" class="image">
-        <span class="removeImg" @click="removeImage($event)">
-          <font-awesome-icon :icon="['fas', 'times']"/>
+      <div
+        v-for="(image, index) in spot.images"
+        :id="image.timestamp?.toString()"
+        :key="image.timestamp"
+        class="image"
+      >
+        <span class="removeImg" @click="removeImage" :id="index.toString()">
+          <font-awesome-icon :icon="['fas', 'times']" />
         </span>
-        <img :src="image.source" alt="A very nice picture"/>
+        <img :src="image.source" alt="A very nice picture" />
       </div>
       <span class="addImg" @click="triggerFileInput">
-      <font-awesome-icon :icon="['fas', 'plus']" size="2x"/>
+        <font-awesome-icon :icon="['fas', 'plus']" size="2x" />
       </span>
     </div>
-    <input type="file" accept="image/*" multiple @change="onFileChanged($event)" ref="imageUpload" v-show="false"/>
+    <input
+      type="file"
+      accept="image/*"
+      multiple
+      @change="onFileChanged($event)"
+      ref="imageUpload"
+      v-show="false"
+    />
 
     <div class="description">
       <p>Beschreibung</p>
-      <textarea v-model="spot.description" placeholder="Tell us what you did here!"/>
+      <textarea v-model="spot.description" placeholder="Tell us what you did here!" />
     </div>
 
     <div class="submit">
       <p class="status">{{ status }}</p>
       <button @click="submit">Hochladen</button>
     </div>
-
   </main>
 </template>
 
 <script setup lang="ts">
-import {onMounted, ref} from "vue";
-import type {NewSpot} from "trippify-client";
-import {useTripStore} from "@/stores/trip";
-import router from "@/router";
-import SmallMap from "@/components/SmallMap.vue";
-import Compressor from 'compressorjs';
+import { onMounted, ref } from 'vue'
+import type { NewSpot } from 'trippify-client'
+import { useTripStore } from '@/stores/trip'
+import router from '@/router'
+import SmallMap from '@/components/SmallMap.vue'
+import Compressor from 'compressorjs'
 
 /* Lat/Lon accuracy by decimal places:
-* 3 decimal places: ~111 meters
-* 4 decimal places: ~11.1 meters
-* 5 decimal places: ~1.11 meters
-*/
+ * 3 decimal places: ~111 meters
+ * 4 decimal places: ~11.1 meters
+ * 5 decimal places: ~1.11 meters
+ */
 const POSITIONING_ACCURACY = 1000 // -> 11.1m
 
 const COMPRESSION_FACTOR = 0.2
@@ -66,12 +81,12 @@ const COMPRESSION_FACTOR = 0.2
 const tripStore = useTripStore()
 
 const spot = ref<NewSpot>({
-  title: "",
+  title: '',
   latitude: 0,
   longitude: 0,
   date_from: new Date().toISOString(),
-  description: "",
-  images: [],
+  description: '',
+  images: []
 })
 
 const imageUpload = ref<HTMLInputElement | null>(null)
@@ -82,49 +97,44 @@ function triggerFileInput() {
   }
 }
 
-const status = ref<string>(" ")
-
+const status = ref<string>(' ')
 
 function goBack() {
   router.back()
 }
 
-onMounted(() =>
-    getLocation()
-)
-
+onMounted(() => getLocation())
 
 function getLocation() {
   if (window.navigator.geolocation) {
-    console.log("Getting location...")
-    window.navigator.geolocation.getCurrentPosition(
-        (position: GeolocationPosition) => {
-          const lat = Math.round(position.coords.latitude * POSITIONING_ACCURACY) / POSITIONING_ACCURACY
-          const lon = Math.round(position.coords.longitude * POSITIONING_ACCURACY) / POSITIONING_ACCURACY
+    console.log('Getting location...')
+    window.navigator.geolocation.getCurrentPosition((position: GeolocationPosition) => {
+      const lat = Math.round(position.coords.latitude * POSITIONING_ACCURACY) / POSITIONING_ACCURACY
+      const lon =
+        Math.round(position.coords.longitude * POSITIONING_ACCURACY) / POSITIONING_ACCURACY
 
-          spot.value.latitude = lat
-          spot.value.longitude = lon
-        }
-    )
+      spot.value.latitude = lat
+      spot.value.longitude = lon
+    })
   } else {
-    alert("Geolocation is not supported")
-    console.log("Geolocation is not supported");
+    alert('Geolocation is not supported')
+    console.log('Geolocation is not supported')
   }
 }
 
 async function onFileChanged($event: Event) {
-  console.log("File uploaded")
-  const target = $event.target as HTMLInputElement;
+  console.log('File uploaded')
+  const target = $event.target as HTMLInputElement
   if (target && target.files) {
     for (let file of target.files) {
       const compressedImage = await compressImage(file, COMPRESSION_FACTOR)
-      const reader = new FileReader();
-      reader.readAsDataURL(compressedImage);
+      const reader = new FileReader()
+      reader.readAsDataURL(compressedImage)
       reader.onloadend = () => {
         if (reader.readyState === 2 && reader.result) {
           spot.value.images.push({
             source: reader.result.toString(),
-            timestamp: get32BitTimestamp(),
+            timestamp: get32BitTimestamp()
           })
         }
       }
@@ -137,39 +147,39 @@ async function compressImage(image: File, quality: number): Promise<File | Blob>
     new Compressor(image, {
       quality,
       success: resolve,
-      error: reject,
-    });
-  });
-
+      error: reject
+    })
+  })
 }
-
 
 function get32BitTimestamp() {
-  const milliseconds = Date.now();
-  let seconds = Math.floor(milliseconds / 1000);
+  const milliseconds = Date.now()
+  let seconds = Math.floor(milliseconds / 1000)
 
-  return seconds & 0xFFFFFFFF;
+  return seconds & 0xffffffff
 }
 
-function removeImage($event: Event) { // TODO: fix
-  const target = $event.target as HTMLImageElement;
-  const timestamp = Number(target.id)
-  const newArr = spot.value.images.filter((image) => image.timestamp !== timestamp)
-  spot.value.images = newArr
+function removeImage(event: Event) {
+  const target = event.currentTarget as HTMLSpanElement
+  const index = Number(target.id)
+  spot.value.images.splice(index, 1)
 }
 
 function submit() {
   const tripId = tripStore.trip._id
 
-  if (spot.value.title.length > 0 && spot.value.latitude != 0 && spot.value.longitude != 0 && spot.value.images.length > 0) {
-    tripStore.addSpotToTrip(tripId, spot.value).then(
-        () => window.location.href = "/"
-    )
+  if (
+    spot.value.title.length > 0 &&
+    spot.value.latitude != 0 &&
+    spot.value.longitude != 0 &&
+    spot.value.images.length > 0
+  ) {
+    tripStore.addSpotToTrip(tripId, spot.value).then(() => (window.location.href = '/'))
   } else {
-    status.value = "Please fill out all fields!"
+    status.value = 'Please fill out all fields!'
 
     setTimeout(() => {
-      status.value = " "
+      status.value = ' '
     }, 3000)
   }
 }
@@ -201,7 +211,6 @@ main {
       gap: 0.5rem;
       justify-self: flex-start;
     }
-
 
     p {
       font-family: Arvo;
@@ -261,7 +270,7 @@ main {
       font-family: Arvo;
       font-size: 1rem;
       font-weight: 900;
-      color: #D9D9D9;
+      color: #d9d9d9;
 
       animation: loading 1.5s infinite linear;
     }
@@ -329,7 +338,7 @@ main {
       height: 100%;
       aspect-ratio: 2/3;
       border-radius: 20px;
-      background-color: #D9D9D9;
+      background-color: #d9d9d9;
 
       display: flex;
       align-items: center;
@@ -349,7 +358,7 @@ main {
     height: 20vh;
     padding: 1rem;
 
-    background-color: #D9D9D9;
+    background-color: #d9d9d9;
     color: var(--color-text);
     font-family: Lato;
     font-size: 1rem;
@@ -366,7 +375,7 @@ main {
       width: 100%;
       height: 80%;
 
-      background-color: #D9D9D9;
+      background-color: #d9d9d9;
       font-family: Lato;
 
       font-size: 1rem;
