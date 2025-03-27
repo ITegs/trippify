a4412a9a7c9feb24c6c0db17f3ab742821605e96
Johannes Pahle <joe@jo-dev.de>
2025-03-27 22:20:47 +0100

    Improve login page

--- frontend/src/views/AccountView.vue
+++ frontend/src/views/AccountView.vue
@@ -20,6 +20,11 @@ function logout(event: Event) {
 
 <template>
   <main>
+    <h1>
+      <span v-for="(letter, i) in 'trippify'" :key="i" :style="`animation-delay: -${1 - i * 0.1}s`">
+        {{ letter }}
+      </span>
+    </h1>
     <form v-if="!userStore.user.username" @submit="login" class="login">
       <input name="username" type="text" v-model="username" placeholder="Username" />
       <input name="password" type="password" v-model="password" placeholder="Password" />
@@ -29,6 +34,15 @@ function logout(event: Event) {
       Logged in as {{ userStore.user.username }}
       <button>Logout</button>
     </form>
+    <p>
+      Hier log ich mich ein :)
+      <br />
+      <br />
+      Profilerstellung kommt erst in der v3. Sorry!
+      <br />
+      Zum Trost eine lustige Katze:
+    </p>
+    <img src="/cat.gif" alt="Funny cat" />
   </main>
 </template>
 
@@ -38,19 +52,44 @@ main {
   width: 100%;
   padding-inline: 1rem;
   display: flex;
+  flex-direction: column;
   align-items: center;
   justify-content: center;
+
+  h1 {
+    font-family: Arvo;
+    color: var(--color-primary);
+    font-weight: 900;
+    font-size: 1.5rem;
+    margin-bottom: 2rem;
+    border: 2px solid var(--color-secondary);
+    padding: 0.5rem 1rem;
+    border-radius: 0.2em;
+    transform: skewX(10deg);
+
+    span {
+      display: inline-block;
+      animation: wave 2s ease-in-out infinite;
+    }
+  }
+
+  p {
+    font-family: Arvo;
+    text-align: center;
+    margin-bottom: 1rem;
+  }
 
   form {
     display: flex;
     flex-direction: column;
     gap: 0.5rem;
+    margin-bottom: 3rem;
 
     input {
-      border: 3px solid var(--color-secondary);
+      border: 2px solid var(--color-secondary);
       border-radius: 10px;
       padding: 0.5rem 1rem;
-      font-size: 1.3rem;
+      font-size: 1rem;
       font-weight: 600;
     }
 
@@ -60,8 +99,18 @@ main {
       border: none;
       border-radius: 10px;
       padding: 0.5rem 1rem;
-      font-size: 1.3rem;
+      font-size: 1rem;
       font-weight: 600;
+    }
+  }
+
+  @keyframes wave {
+    0%,
+    100% {
+      transform: translateY(-3px);
+    }
+    50% {
+      transform: translateY(3px);
     }
   }
 }
